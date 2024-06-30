a6c607256dc081f3cd0d8ef5998387ad712e79ca
Akhilpraka5h <134758922+Akhilpraka5h@users.noreply.github.com>
2024-06-30 17:08:05 +0530

    projects updatd

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -15,21 +15,15 @@ const Projects = () => {
             <div className="layer">
               <h3>Netflix Clone</h3>
               <p>
-                Netflix App is a dynamic web application built using React JS,
-                showcasing my proficiency in modern web development. Leveraging
-                the powerful TMDB (The Movie Database) API, the app provides
-                users with an extensive catalog of movies and TV shows, complete
-                with detailed information and engaging visuals. The application
-                incorporates Firebase Authentication to ensure secure and
-                seamless user login and registration. This integration not only
-                enhances the user experience but also demonstrates my ability to
-                implement robust authentication systems.
+                Netflix App: Developed with React JS, integrating the TMDB API
+                for a comprehensive movie and TV show catalog. Implemented
+                Firebase Authentication for secure user login and registration.
               </p>
               <a
                 href="https://github.com/Akhilpraka5h/Netflix_clone"
                 target="_blanks"
               >
-               <i className="fa-solid fa-arrow-up-right-from-square"></i>
+                <i className="fa-solid fa-arrow-up-right-from-square"></i>
               </a>
             </div>
           </div>
@@ -38,17 +32,11 @@ const Projects = () => {
             <div className="layer">
               <h3>Xomato</h3>
               <p>
-                Xomato is an innovative React.js project designed to serve as a
-                full-fledged food delivery website, drawing inspiration from the
-                popular Zomato platform. This project integrates the powerful
-                Stripe API for seamless and secure payment processing, ensuring
-                a smooth transaction experience for users. The backend is
-                powered by MongoDB, providing a robust and scalable database
-                solution to handle user data, order information, and restaurant
-                details efficiently. Xomato combines a modern, responsive user
-                interface with efficient functionality, making it an ideal
-                solution for aspiring entrepreneurs looking to launch their own
-                food delivery service.
+                Xomato: A React.js project inspired by Zomato, serving as a
+                full-fledged food delivery website. Integrated Stripe API for
+                secure payment processing. Utilized MongoDB for robust and
+                scalable backend handling user data, orders, and restaurant
+                details.
               </p>
               <a
                 href="https://xomato-fooddelivery-frontend.onrender.com"
@@ -63,15 +51,12 @@ const Projects = () => {
             <div className="layer">
               <h3>Fitness Web</h3>
               <p>
-                Fitness Web is designed to provide an optimal viewing experience
-                across all devices, from desktops to smartphones. Featuring a
-                sleek and modern design, it showcases gym services, class
-                schedules, trainer profiles, and membership options. The
-                user-friendly interface ensures easy navigation, allowing
-                visitors to book classes, view workout plans, and access fitness
-                resources effortlessly. Whether users are at home or on the go,
-                our responsive webpage ensures they stay connected and engaged
-                with our gym offerings.
+                Fitness Web: Designed for optimal viewing on all devices,
+                showcasing gym services, class schedules, trainer profiles, and
+                membership options. Features a sleek, modern design with a
+                user-friendly interface for easy navigation. Allows users to
+                book classes, view workout plans, and access fitness resources
+                effortlessly.
               </p>
               <a
                 href="https://github.com/Akhilpraka5h/gym-website"
@@ -86,14 +71,10 @@ const Projects = () => {
             <div className="layer">
               <h3>Gemini Ai</h3>
               <p>
-                Gemini Ai clone, built with React.js and powered by the Gemini
-                API, delivers an interactive and intuitive chat experience
-                across all devices. This modern web app combines the flexibility
-                of React with the robust capabilities of Gemini API to provide
-                real-time, AI-driven conversations. The sleek design and
-                responsive layout ensure seamless usability on desktops,
-                tablets, and smartphones, making it easy for users to engage
-                with ChatGPT anytime, anywhere.
+                Gemini AI Clone: Built with React.js and powered by the Gemini
+                API for an interactive chat experience across all devices.
+                Provides real-time, AI-driven conversations with a sleek design
+                and responsive layout.
               </p>
               <a href="https://chatbot-gemini-ai.netlify.app" target="_blank">
                 <i className="fa-solid fa-arrow-up-right-from-square"></i>
