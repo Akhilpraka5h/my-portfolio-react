ef77a45732de3de773092a6cfda9816d1b50c9fb
Akhilpraka5h <134758922+Akhilpraka5h@users.noreply.github.com>
2024-07-01 11:46:54 +0530

    resume update

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,8 +1,14 @@
+import React, { useRef } from "react";
 import "./Hero.css";
 import profileImg from "../../assets/Profile_Hero.png";
 import resume from "../../assets/Akhil resume_portfolio.pdf";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 const Hero = () => {
+  const linkRef = useRef(null);
+
+  const handleDivClick = () => {
+    linkRef.current.click();
+  };
   return (
     <div className="hero" id="home">
       <img src={profileImg} alt="" />
@@ -16,8 +22,10 @@ const Hero = () => {
             Connect with me
           </AnchorLink>
         </div>
-        <div className="heroResume">
-          <a href={resume}>Resume</a>
+        <div className="heroResume" onClick={handleDivClick}>
+          <a href={resume} download ref={linkRef}>
+            Resume
+          </a>
         </div>
       </div>
     </div>
@@ -25,3 +33,41 @@ const Hero = () => {
 };
 
 export default Hero;
+
+// import React, { useRef } from "react";
+// import "./Hero.css";
+// import profileImg from "../../assets/Profile_Hero.png";
+// import resume from "../../assets/Akhil resume_portfolio.pdf";
+// import AnchorLink from "react-anchor-link-smooth-scroll";
+
+// const Hero = () => {
+//   const linkRef = useRef(null);
+
+//   const handleDivClick = () => {
+//     linkRef.current.click();
+//   };
+
+//   return (
+//     <div className="hero" id="home">
+//       <img src={profileImg} alt="" />
+//       <h1>
+//         <span>I'm Akhil,</span> Passionate Software Engineer.
+//       </h1>
+//       <p>A Passionate Software Engineer and a recent MCA Graduate from KTU.</p>
+//       <div className="heroAction">
+//         <div className="heroConnect">
+//           <AnchorLink href="#contact" className="anchorLink">
+//             Connect with me
+//           </AnchorLink>
+//         </div>
+//         <div className="heroResume" onClick={handleDivClick}>
+//           <a href={resume} download ref={linkRef}>
+//             Resume
+//           </a>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Hero;
